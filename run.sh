824937564363208e696f4371151b65614fe1b300
Michal Medvecky <misko@MacBook.local>
2017-01-02 22:38:46 +0100

    typo

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ ulimit -l unlimited
 # Ackee: add backup credentials to ES config dir
 
 echo -e "$GS_CRED_FILE_CONTENT" > /elasticsearch/config/gs.json
-chown elasticsearc /elasticsearch/config/gs.json
+chown elasticsearch /elasticsearch/config/gs.json
 chmod 600 /elasticsearch/config/gs.json
 
 # run
