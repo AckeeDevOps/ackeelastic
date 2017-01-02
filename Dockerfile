6905bb6856e081d2c94ff385d778fd5ea6776a78
Michal Medvecky <misko@MacBook.local>
2017-01-02 22:05:05 +0100

    add credentials file

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,5 @@ FROM quay.io/pires/docker-elasticsearch-kubernetes:5.1.1
 
 RUN /elasticsearch/bin/elasticsearch-plugin install analysis-icu
 RUN /elasticsearch/bin/elasticsearch-plugin install -bs repository-gcs 
+COPY run.sh /
+RUN chmod 755 /run.sh
---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# provision elasticsearch user
+addgroup sudo
+adduser -D -g '' elasticsearch
+adduser elasticsearch sudo
+chown -R elasticsearch /elasticsearch /data
+echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+
+# allow for memlock
+ulimit -l unlimited
+
+# Ackee: add backup credentials to ES config dir
+
+echo $GS_CRED_FILE_CONTENT > /elasticsearch/config/gs.json
+chown elasticsearc /elasticsearch/config/gs.json
+chmod 600 /elasticsearch/config/gs.json
+
+# run
+sudo -E -u elasticsearch /elasticsearch/bin/elasticsearch
+
